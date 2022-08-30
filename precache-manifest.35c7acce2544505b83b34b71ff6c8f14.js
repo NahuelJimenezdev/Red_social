63e3604090d800e9c92575c16ffffbfaa7c4e460
NahuelJimenezDev <102840530+NahuelJimenezdev@users.noreply.github.com>
2022-08-30 04:50:28 -0300

    Add files via upload

---
+++ precache-manifest.35c7acce2544505b83b34b71ff6c8f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d94b4962d785e9c958",
+    "url": "/static/css/main.76898b24.chunk.css"
+  },
+  {
+    "revision": "41d94b4962d785e9c958",
+    "url": "/static/js/main.41d94b49.chunk.js"
+  },
+  {
+    "revision": "a438bfcf2c55e5954ebc",
+    "url": "/static/js/1.a438bfcf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e7161369640bbffc9a12288de09511b",
+    "url": "/static/media/RedSocial_Perfil_1.6e716136.png"
+  },
+  {
+    "revision": "769f4db4887142a2701fc4c6fe0c9c71",
+    "url": "/index.html"
+  }
+];
